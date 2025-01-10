9082740a660fa6f901239c3ee618adb586de0991
IWLeng <143839160+IWLeng@users.noreply.github.com>
2025-01-10 15:44:47 -0300

    Create rules_library_proofpoint.py

---
+++ libraries/Proofpoint/rules_library_proofpoint.py
@@ -0,0 +1,207 @@
+rule_sets = {
+    "1": {
+        "name": "cs-cz",
+        "rules": [
+            (r'\bDocuSign\b', r'<phoneme alphabet="ipa" ph="dokusaːjn">DocuSign</phoneme>'),
+            (r'\be-mailech\b', r'<phoneme alphabet="ipa" ph="iːmaɪlɛx">e-mailech</phoneme>'),
+            (r'\be-mailová\b', r'<phoneme alphabet="ipa" ph="iːmaɪlovaː">e-mailová</phoneme>'),
+            (r'\be-mailové\b', r'<phoneme alphabet="ipa" ph="iːmaɪlovɛː">e-mailové</phoneme>'),
+            (r'\be-mailovém\b', r'<phoneme alphabet="ipa" ph="iːmaɪlovɛːm">e-mailovém</phoneme>'),
+            (r'\be-mailových\b', r'<phoneme alphabet="ipa" ph="iːmaɪloviːx">e-mailových</phoneme>'),
+            (r'\bcloudovými\b', r'<phoneme alphabet="ipa" ph="klaudoviːmi">cloudovými</phoneme>'),
+            (r'\bcloudové\b', r'<phoneme alphabet="ipa" ph="klou̯dovɛː">cloudové</phoneme>'),
+            (r'\bmalwarem\b', r'<phoneme alphabet="ipa" ph="malvɛːrɛm">malwarem</phoneme>'),
+            (r'\bmalwaru\b', r'<phoneme alphabet="ipa" ph="malvɛːru">malwaru</phoneme>'),
+            (r'\bověřte\b', r'<phoneme alphabet="ipa" ph="ovjɛr̝tɛ">ověřte</phoneme>'),
+            (r'\bransonwaru\b', r'<phoneme alphabet="ipa" ph="raŋsomvɛːru">ransonwaru</phoneme>'),
+            (r'\bChat\b', r'<phoneme alphabet="ipa" ph="t͡ʃɛt">Chat</phoneme>'),
+            (r'\bchatové\b', r'<phoneme alphabet="ipa" ph="t͡ʃɛtovɛː">chatové</phoneme>'),
+            (r'\bGmailu\b', r'<phoneme alphabet="ipa" ph="ˈd͡ʒiːmɛjlu">Gmailu</phoneme>'),
+            (r'\bAI\b', r'<phoneme alphabet="ipa" ph="ˈɛj ˈaːj">AI</phoneme>'),
+            (r'\bscreeningů\b', r'<phoneme alphabet="ipa" ph="ˈskriːnɪnguː">screeningů</phoneme>'),
+        ]
+    },
+    "2": {
+        "name": "de-de",
+        "rules": [
+            (r'\bbot\b', r'<phoneme alphabet="ipa" ph="bɔt">bot</phoneme>'),
+            (r'\bbotnet\b', r'<phoneme alphabet="ipa" ph="bˈɔtnət">botnet</phoneme>'),
+            (r'\bEngineering\b', r'<phoneme alphabet="ipa" ph="ˈɛndʒəˈnɪɹɪŋ">Engineering</phoneme>'),
+            (r'\bPhishing\b', r'<phoneme alphabet="ipa" ph="ˈfɪʃɪŋ">Phishing</phoneme>'),
+            (r'\bgepostet\b', r'<phoneme alphabet="ipa" ph="ɡəpˈɔstət">gepostet</phoneme>'),
+            (r'\bHijacking\b', r'<phoneme alphabet="ipa" ph="ˈhaɪˌdʒækɪŋ">Hijacking</phoneme>'),
+            (r'\bKeylogger\b', r'<phoneme alphabet="ipa" ph="kˈiːlɒɡə">Keylogger</phoneme>'),
+            (r'\bKryptojacking\b', r'<phoneme alphabet="ipa" ph="kɾˈyptoː d͡ʒakɪŋ">Kryptojacking</phoneme>'),
+            (r'\bMining\b', r'<phoneme alphabet="ipa" ph="mˈa͡ɪnɪŋ">Mining</phoneme>'),
+            (r'\bMighty\b', r'<phoneme alphabet="ipa" ph="mˈa͡ɪti">Mighty</phoneme>'),
+            (r'\bMalspam\b', r'<phoneme alphabet="ipa" ph="mˈalspam">Malspam</phoneme>'),
+            (r'\bSpam\b', r'<phoneme alphabet="ipa" ph="spˈam">Spam</phoneme>'),
+            (r'\bSpambot\b', r'<phoneme alphabet="ipa" ph="spˈambɒt">Spambot</phoneme>'),
+            (r'\bSpambots\b', r'<phoneme alphabet="ipa" ph="spˈambɒts">Spambots</phoneme>'),
+            (r'\bVirtuosin\b', r'<phoneme alphabet="ipa" ph="vˌɪɾtuːoːzˈiːn">Virtuosin</phoneme>'),
+            (r'\büblen\b', r'<phoneme alphabet="ipa" ph="ˈyblən">üblen</phoneme>'),        
+        ]
+    },
+    "3": {
+        "name": "en-au",
+        "rules": [
+            (r'\bAiTM\b', r'<say-as interpret-as="characters">AiTM</say-as>'),
+        ]
+    },
+    "4": {
+        "name": "en-gb",
+        "rules": [
+            (r'\bAiTM\b', r'<say-as interpret-as="characters">AiTM</say-as>'),
+        ]
+    },
+    "5": {
+        "name": "es-es",
+        "rules": [
+            (r'\bdeep-fake\b', r'<phoneme alphabet="ipa" ph="diːp.feɪk">deep-fake</phoneme>'),
+            (r'\bdeep-fakes\b', r'<phoneme alphabet="ipa" ph="diːp.feɪks">deep-fakes</phoneme>'),
+            (r'\bMalware\b', r'<phoneme alphabet="ipa" ph="ˈmæl.weɹ">Malware</phoneme>'),
+            (r'\bmalvertising\b', r'<phoneme alphabet="ipa" ph="mal.ver.taɪzɪŋ">malvertising</phoneme>'),
+            (r'\bSpambots\b', r'<phoneme alphabet="ipa" ph="spˈambots">Spambots</phoneme>'),
+            (r'\bstealers\b', r'<phoneme alphabet="ipa" ph="stˈilers">stealers</phoneme>'),
+            (r'\bhackean\b', r'<phoneme alphabet="ipa" ph="xa.kˈe.ˌan">hackean</phoneme>'),
+            (r'\bhackear\b', r'<phoneme alphabet="ipa" ph="xa.kˈe.ˌar">hackear</phoneme>'),
+            (r'\bhackeado\b', r'<phoneme alphabet="ipa" ph="xakeado">hackeado</phoneme>'),
+            (r'\bhackear\b', r'<phoneme alphabet="ipa" ph="xakear">hackear</phoneme>'),
+            (r'\bShell\b', r'<phoneme alphabet="ipa" ph="ʒɛl">Shell</phoneme>'),
+        ]
+    },
+    "6": {
+        "name": "es-mx",
+        "rules": [
+            (r'\bdeepfake\b', r'<phoneme alphabet="ipa" ph="diːp.feɪk">deep-fake</phoneme>'),
+            (r'\bdeepfakes\b', r'<phoneme alphabet="ipa" ph="diːp.feɪks">deep-fakes</phoneme>'),
+            (r'\bmalvertising\b', r'<phoneme alphabet="ipa" ph="mal.ver.taɪzɪŋ">malvertising</phoneme>'),
+            (r'\bSmishing\b', r'<phoneme alphabet="ipa" ph="smɪʎɪŋ">Smishing</phoneme>'),
+            (r'\bhackean\b', r'<phoneme alphabet="ipa" ph="xa.kˈe.ˌan">hackean</phoneme>'),
+            (r'\bhackeada\b', r'<phoneme alphabet="ipa" ph="xakeada">hackeada</phoneme>'),
+            (r'\bhackeado\b', r'<phoneme alphabet="ipa" ph="xakeado">hackeado</phoneme>'),
+            (r'\bhackeados\b', r'<phoneme alphabet="ipa" ph="xakeados">hackeados</phoneme>'),
+            (r'\bShell\b', r'<phoneme alphabet="ipa" ph="ʒɛl">Shell</phoneme>'),
+            (r'\bphishing\b', r'Fishing'),
+        ]
+    },
+    "7": {
+        "name": "fr-ca",
+        "rules": [
+            (r'\bzombie\b', r'<phoneme alphabet="ipa" ph=" zo.bjə">zombie</phoneme>'),
+            (r'\bzombies\b', r'<phoneme alphabet="ipa" ph=" zo.bjə">zombies</phoneme>'),
+            (r'\bbot\b', r'<phoneme alphabet="ipa" ph="bɔt">bot</phoneme>'),
+            (r'\binfonuagique\b', r'<phoneme alphabet="ipa" ph="ɛ̃fonyaʒik">infonuagique</phoneme>'),
+            (r'\bLinkedIn\b', r'<phoneme alphabet="ipa" ph="lɪŋkt.in">LinkedIn</phoneme>'),
+            (r'\bmarketing\b', r'<phoneme alphabet="ipa" ph="maʁkɛtɛiɲ">marketing</phoneme>'),
+            (r'\bpuppet\b', r'<phoneme alphabet="ipa" ph="pohpɛt">puppet</phoneme>'),
+            (r'\bcinq secondes\b', r'<phoneme alphabet="ipa" ph="sɛ̃k søgɔ̃d">cinq secondes</phoneme>'),
+            (r'\bsûr\b', r'<phoneme alphabet="ipa" ph="syʁ">sûr</phoneme>'),
+            (r'\bregardez y\b', r'<phoneme alphabet="ipa" ph="ɾəgaɾdez‿ i">regardez y</phoneme>'),
+            (r'\bWi-Fi\b', r'<phoneme alphabet="ipa" ph="wifi">Wi-Fi</phoneme>'),
+        ]
+    },
+    "8": {
+        "name": "fr-fr",
+        "rules": [
+            (r'\bzombies\b', r'<phoneme alphabet="ipa" ph=" zo.bjə">zombies</phoneme>'),
+            (r'\bdeep-fake\b', r'<phoneme alphabet="ipa" ph="diːp.feɪk">deep-fake</phoneme>'),
+            (r'\bdeep-fakes\b', r'<phoneme alphabet="ipa" ph="diːp.feɪks">deep-fakes</phoneme>'),
+            (r'\bcryptomonnaie\b', r'<phoneme alphabet="ipa" ph="kʁiptɔmɔnɛ">cryptomonnaie</phoneme>'),
+            (r'\bregardez y\b', r'<phoneme alphabet="ipa" ph="ɾəgaɾdez‿ i">regardez y</phoneme>'),
+        ]
+    },
+    "9": {
+        "name": "it-it",
+        "rules": [
+            (r'\bleggere\b', r'<phoneme alphabet="ipa" ph="ˈledˈdʒɛre">leggere</phoneme>'),
+            (r'\bcomplete\b', r'<phoneme alphabet="ipa" ph="ˈkomˈplɛte">complete</phoneme>'),
+            (r'\bfile\b', r'<phoneme alphabet="ipa" ph="fajl">file</phoneme>'),
+        ]
+    },
+    "10": {
+        "name": "pl-pl",
+        "rules": [
+            (r'\bpo to\b', r'<phoneme alphabet="ipa" ph="ˈpɔ.tɔ">po to</phoneme>'),
+            (r'\bWord\b', r'<phoneme alphabet="ipa" ph="ˈword">Word</phoneme>'),
+            (r'\bSMS-a\b', r'<phoneme alphabet="ipa" ph="ɛsɛˈmɛsa">SMS-a</phoneme>'),
+            (r'\bZignorowania\b', r'<phoneme alphabet="ipa" ph="zignorovanja">Zignorowania</phoneme>'),
+            (r'\bAiTM\b', r'<phoneme alphabet="ipa" ph="ˈaj.te.em">AiTM</phoneme>'),
+            (r'\broutery\b', r'<phoneme alphabet="ipa" ph="ˈru.tery">routery</phoneme>'),
+        ]
+    },
+    "11": {
+        "name": "pt-br",
+        "rules": [
+            (r'\bbotnet\b', r'<phoneme alphabet="ipa" ph="bɒt.ˈnɛt">botnet</phoneme>'),
+            (r'\bdeepfake\b', r'<phoneme alphabet="ipa" ph="ˈdiːp.feɪk">deepfake</phoneme>'),
+            (r'\bcandidate-se\b', r'<phoneme alphabet="ipa" ph="kɐ̃dʒiˈdat͡ʃisi">candidate-se</phoneme>'),
+            (r'\bcrypto\b', r'<phoneme alphabet="ipa" ph="ˈkɹɪptʊ">crypto</phoneme>'),
+            (r'\bMalware\b', r'<phoneme alphabet="ipa" ph="ˈmæl.weɹ">Malware</phoneme>'),
+            (r'\bmalvertising\b', r'<phoneme alphabet="ipa" ph="mælˈvərˌtaɪzɪŋ">malvertising</phoneme>'),
+            (r'\bMighty \b', r'<phoneme alphabet="ipa" ph="ˈmaɪti">Mighty </phoneme>'),
+            (r'\bmalspam\b', r'<phoneme alphabet="ipa" ph="malspam">malspam</phoneme>'),
+            (r'\bpaguem\b', r'<phoneme alphabet="ipa" ph="ˈpaɡej">paguem</phoneme>'),
+            (r'\bCFO\b', r'<phoneme alphabet="ipa" ph="ˌsiː.ɛfˈoʊ">CFO</phoneme>'),
+            (r'\bSpambot\b', r'<phoneme alphabet="ipa" ph="spˈambɒt">Spambot</phoneme>'),
+            (r'\bspear\b', r'<phoneme alphabet="ipa" ph="spɪr">spear</phoneme>'),
+            (r'\bStealers\b', r'<phoneme alphabet="ipa" ph="stˈiːlexs">Stealers</phoneme>'),
+            (r'\bsozinhos\b', r'<phoneme alphabet="ipa" ph="sozˈiˌɲus">sozinhos</phoneme>'),
+            (r'\bcoloca-se\b', r'<phoneme alphabet="ipa" ph="ko.ˈlɔ.ka.si">coloca-se</phoneme>'),
+        ]
+    },
+    "12": {
+        "name": "pt-pt",
+        "rules": [
+            (r'\bbotnet\b', r'<phoneme alphabet="ipa" ph="bɒtˈnɛt">botnet</phoneme>'),
+            (r'\bDocuSign\b', r'<phoneme alphabet="ipa" ph="dɑkjˈsaɪn">DocuSign</phoneme>'),
+            (r'\bguardá-las\b', r'<phoneme alphabet="ipa" ph="guɐɾdaˈlɐʃ">guardá-las</phoneme>'),
+            (r'\bredirecionar\b', r'<phoneme alphabet="ipa" ph="ʀədiɾɛsiˈunaɾ">redirecionar</phoneme>'),
+            (r'\bcibercriminosos\b', r'<phoneme alphabet="ipa" ph="si.bɨɾ.kɾɨ.mi.ˈno.zuʃ">cibercriminosos</phoneme>'),
+            (r'\bspear\b', r'<phoneme alphabet="ipa" ph="spɪə">spear</phoneme>'),
+        ]
+    },
+    "13": {
+        "name": "th-th",
+        "rules": [
+            (r'\bแอปพลิเคชัน\b', r'<phoneme alphabet="ipa" ph="ˌæplɪˈkeɪʃən">แอปพลิเคชัน</phoneme>'),
+            (r'\bDocuSign\b', r'<phoneme alphabet="ipa" ph="ˈdɑkiʊsɑin">DocuSign</phoneme>'),
+            (r'\bโคร\b', r'<phoneme alphabet="ipa" ph="kʰrōː">โคร</phoneme>'),
+            (r'\bมาโคร\b', r'<phoneme alphabet="ipa" ph="maːkʰrō">มาโคร</phoneme>'),
+            (r'\bรูปแบบอื่น\b', r'<phoneme alphabet="ipa" ph="rú̋̀p.bàb.eun">รูปแบบอื่น</phoneme>'),
+            (r'\bTOAD\b', r'<phoneme alphabet="ipa" ph="toad">TOAD</phoneme>'),
+        ]
+    },
+    "14": {
+        "name": "tr-tr",
+        "rules": [
+            (r'\bAiTM\b', r'<phoneme alphabet="ipa" ph="a.i.te.me">AiTM</phoneme>'),
+            (r'\bASECNA\b', r'<phoneme alphabet="ipa" ph="a‿sekˈ.na">ASECNA</phoneme>'),
+            (r'\bbeklenmedik\b', r'<phoneme alphabet="ipa" ph="beklenmeˈdik">beklenmedik</phoneme>'),
+            (r'\bbeklenmeyen\b', r'<phoneme alphabet="ipa" ph="beklenmeˈjen">beklenmeyen</phoneme>'),
+            (r'\bDeepfake\b', r'<phoneme alphabet="ipa" ph="diip.feejk">Deepfake</phoneme>'),
+            (r'\bDocuSign\b', r'<phoneme alphabet="ipa" ph="doˈkjuːsajn">DocuSign</phoneme>'),
+            (r'\be-postaya\b', r'<phoneme alphabet="ipa" ph="e.pos.ta.ja">e-postaya</phoneme>'),
+            (r'\be-postasını\b', r'<phoneme alphabet="ipa" ph="e.pos.ta.sɯ.nɯ">e-postasını</phoneme>'),
+            (r'\be-posta\b', r'<phoneme alphabet="ipa" ph="eːposta">e-posta</phoneme>'),
+            (r'\be-postadan\b', r'<phoneme alphabet="ipa" ph="eːpostadan">e-postadan</phoneme>'),
+            (r'\bE-postalar\b', r'<phoneme alphabet="ipa" ph="eːpostalar">E-postalar</phoneme>'),
+            (r'\be-postalara\b', r'<phoneme alphabet="ipa" ph="eːpostalaɾa">e-postalara</phoneme>'),
+            (r'\be-postaları\b', r'<phoneme alphabet="ipa" ph="eːpostalaɾɯ">e-postaları</phoneme>'),
+            (r'\be-postaların\b', r'<phoneme alphabet="ipa" ph="eːpostalaɾɯ">e-postaların</phoneme>'),
+            (r'\be-postalarının\b', r'<phoneme alphabet="ipa" ph="eːpostalarɯn">e-postalarının</phoneme>'),
+            (r'\be-postalarında\b', r'<phoneme alphabet="ipa" ph="eːpostalarɯnda">e-postalarında</phoneme>'),
+            (r'\be-postalarını\b', r'<phoneme alphabet="ipa" ph="eːpostalaɾɯnɯ">e-postalarını</phoneme>'),
+            (r'\be-postanın\b', r'<phoneme alphabet="ipa" ph="eːpostanɯn">e-postanın</phoneme>'),
+            (r'\be-postası\b', r'<phoneme alphabet="ipa" ph="eːpostasɯ">e-postası</phoneme>'),
+            (r'\be-postasından\b', r'<phoneme alphabet="ipa" ph="eːpostasɯndan">e-postasından</phoneme>'),
+            (r'\beşleşmeyen\b', r'<phoneme alphabet="ipa" ph="eʃleʃmejen">eşleşmeyen</phoneme>'),
+            (r'\bhalen\b', r'<phoneme alphabet="ipa" ph="haːlen">halen</phoneme>'),
+            (r'\byalnızca\b', r'<phoneme alphabet="ipa" ph="jal.nɯz̥d͡ʒa">yalnızca</phoneme>'),
+            (r'\bkısaltmalar\b', r'<phoneme alphabet="ipa" ph="kɯsaltmalaɾ">kısaltmalar</phoneme>'),
+            (r'\bolarak\b', r'<phoneme alphabet="ipa" ph="olaɾak">olarak</phoneme>'),
+            (r'\bTabi\b', r'<phoneme alphabet="ipa" ph="ta.biː">Tabi</phoneme>'),
+            (r'\bçözmelerine\b', r'<phoneme alphabet="ipa" ph="t͡ʃœzmeleɾine">çözmelerine</phoneme>'),
+        ]
+    },
+}
