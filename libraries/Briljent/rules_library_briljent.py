b7064512de248fefefc2012c9791f4b01cefa93b
IWLeng <143839160+IWLeng@users.noreply.github.com>
2025-05-08 12:37:29 -0300

    Update rules_library_briljent.py

--- libraries/Briljent/rules_library_briljent.py
+++ libraries/Briljent/rules_library_briljent.py
@@ -1,1 +1,15 @@
-
+rule_sets = {
+    "1": {
+        "name": "Spanish",
+        "rules": [
+          (r'\bStar\b', r'<phoneme alphabet="ipa" ph="stɑr">Star</phoneme>'),
+          (r'\bMenu\b', r'<phoneme alphabet="ipa" ph="ˈmenju">Menu</phoneme>'),
+          (r'\bTranscript\b', r'<phoneme alphabet="ipa" ph="ˈtɹæn.skɹɪpt">Transcript</phoneme>'),
+          (r'\bGlossary\b', r'<phoneme alphabet="ipa" ph="ˈɡlɒs.ə.ɹi">Glossary</phoneme>'),
+          (r'\bResources\b', r'<phoneme alphabet="ipa" ph="ˈri.sɔɹsɪz">Resources</phoneme>'),
+          (r'\bJimmy\b', r'<phoneme alphabet="ipa" ph="ˈd͡ʒɪmi">Jimmy</phoneme>'),
+          (r'\bSTEAM\b', r'<phoneme alphabet="ipa" ph="stiːm">STEAM</phoneme>'),
+          (r'\bKen\b', r'<phoneme alphabet="ipa" ph="kˈen">Ken</phoneme>'),
+        ]
+    }
+}
