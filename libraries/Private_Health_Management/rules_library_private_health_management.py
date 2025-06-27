151f4ed36ce4b03da6f847160ae099e7309861f3
IWLeng <143839160+IWLeng@users.noreply.github.com>
2025-06-27 13:27:42 -0300

    Create rules_library_private_health_management.py

---
+++ libraries/Private_Health_Management/rules_library_private_health_management.py
@@ -0,0 +1,11 @@
+rule_sets = {
+    "1": {
+        "name": "Spanish",
+        "rules": [
+            (r'\bPrivate\b', r'<phoneme alphabet="ipa" ph="ˈpɹaɪ.vɪt">Private</phoneme>'),
+            (r'\bHealth\b', r'<phoneme alphabet="ipa" ph="hɛlf">Health</phoneme>'),
+            (r'\bHealthcare\b', r'<phoneme alphabet="ipa" ph="ˈhɛlfkɛɚ">Healthcare</phoneme>'),
+            (r'\bInsurance\b', r'<phoneme alphabet="ipa" ph="ɪnˈʃʊɹ.əns">Insurance</phoneme>'),
+        ]
+    }
+}
