28d5cc60d54ae83ac7401341d28f2a66d2ef9f3e
IWLeng <143839160+IWLeng@users.noreply.github.com>
2025-06-27 15:07:11 -0300

    Create rules_library_ppg.py

---
+++ libraries/PPG/rules_library_ppg.py
@@ -0,0 +1,12 @@
+rule_sets = {
+    "1": {
+        "name": "French (Canada)",
+        "rules": [
+            (r'\bchevauchement\b', r'<phoneme alphabet="ipa" ph="ʃə.voʃ.mɑ̃">chevauchement</phoneme>'),
+            (r'\bEst\b', r'<phoneme alphabet="ipa" ph="ˈeste">Est</phoneme>'),
+            (r'\btous\b', r'<phoneme alphabet="ipa" ph="tʊs">tous</phoneme>'),
+            (r'\bplus\b', r'<phoneme alphabet="ipa" ph="plus">plus</phoneme>'),
+            (r'\b\b', r''),
+        ]
+    },
+}
