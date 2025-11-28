f5047a3efbf1fc528d80871387e0b138b855e6b1
IWLeng <143839160+IWLeng@users.noreply.github.com>
2025-11-28 15:48:09 -0300

    Create Create rules_library_burkle.py

---
+++ libraries/Burkle/Create rules_library_burkle.py
@@ -0,0 +1,8 @@
+rule_sets = {
+    "1": {
+        "name": "English",
+        "rules": [
+            (r'Bürkle + Schöck', r'<lang xml:lang="de-DE"><phoneme alphabet="ipa" ph="bˈʊɹklə">Bürkle</phoneme> <phoneme alphabet="ipa" ph="ʊnd">+</phoneme> <phoneme alphabet="ipa" ph="ʃøːk">Schöck</phoneme></lang>'),
+        ]
+    },
+}
