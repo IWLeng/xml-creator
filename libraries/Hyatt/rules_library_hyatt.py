0f1c58deec404184800ab3b8b436b76d30691c51
IWLeng <143839160+IWLeng@users.noreply.github.com>
2025-12-02 16:26:49 -0300

    Create rules_library_hyatt.py

---
+++ libraries/Hyatt/rules_library_hyatt.py
@@ -0,0 +1,62 @@
+rule_sets = {
+    "1": {
+        "name": "de-DE",
+        "rules": [
+            (r'\bSuite\b', r'<lang xml:lang="en-US">Suite</lang>'),
+            (r'\bCourtesy Card\b', r'<lang xml:lang="en-US">Courtesy Card</lang>'),
+            (r'\bWorld of Hyatt\b', r'<lang xml:lang="en-US">World of Hyatt</lang>'),
+            (r'\bLifetime\b', r'<lang xml:lang="en-US">Lifetime</lang>'),
+            (r'\bGlobalist\b', r'<lang xml:lang="en-US">Globalist</lang>'),
+            (r'\bDiscoverist\b', r'<lang xml:lang="en-US">Discoverist</lang>'),
+            (r'\bExplorist\b', r'<lang xml:lang="en-US">Explorist</lang>'),
+            (r'\bMember\b', r'<lang xml:lang="en-US">Member</lang>'),
+            (r'\bVacation Club\b', r'<lang xml:lang="en-US">Vacation Club</lang>'),
+            (r'\bHyatt Inclusive Collection\b', r'<lang xml:lang="en-US">Hyatt Inclusive Collection</lang>'),
+            (r'\bAlipay\b', r'<lang xml:lang="en-US">Alipay</lang>'),
+            (r'\bApple Pay\b', r'<lang xml:lang="en-US">Apple Pay</lang>'),
+            (r'\bWeChat Pay\b', r'<lang xml:lang="en-US">WeChat Pay</lang>'),
+            (r'\bHyatt\b', r'<lang xml:lang="en-US"><phoneme alphabet="ipa" ph="ˈhaɪət">Hyatt</phoneme></lang>'),
+            (r'\bMilestone\b', r'<lang xml:lang="en-US"><phoneme alphabet="ipa" ph="majlstɔn">Milestone</phoneme></lang>'),
+            (r'\bMiraval\b', r'<lang xml:lang="en-US"><phoneme alphabet="ipa" ph="miʁaval">Miraval</phoneme></lang>'),
+        ]
+    },
+    "2": {
+        "name": "fr-FR",
+        "rules": [
+                (r'\bWorld of Hyatt\b', r'<lang xml:lang="en-US">World of Hyatt</lang>'),
+                (r'\bLifetime\b', r'<lang xml:lang="en-US">Lifetime</lang>'),
+                (r'Globalist', r'<lang xml:lang="en-US"><phoneme alphabet="ipa" ph="glɔbalist">Globalist</phoneme></lang>'),
+                (r'Discoverist', r'<lang xml:lang="en-US"><phoneme alphabet="ipa" ph="diskɔvøʁist">Discoverist</phoneme></lang>),
+                (r'Explorist', r'<lang xml:lang="en-US"><phoneme alphabet="ipa" ph="ɛksplɔʁist">Explorist</phoneme></lang>),
+                (r'\bMember\b', r'<lang xml:lang="en-US">Member</lang>'),
+                (r'Hyatt', r'<lang xml:lang="en-US"><phoneme alphabet="ipa" ph="ˈhaɪət">Hyatt</phoneme></lang>),
+                (r'\bVacation Club\b', r'<lang xml:lang="en-US">Vacation Club</lang>'),
+                (r'Milestone', r'<lang xml:lang="en-US"><phoneme alphabet="ipa" ph="majlstɔn">Milestone</phoneme></lang>),
+                (r'Miraval', r'<lang xml:lang="en-US"><phoneme alphabet="ipa" ph="miʁaval">Miraval</phoneme></lang>),
+                (r'\bHyatt Inclusive Collection\b', r'<lang xml:lang="en-US">Hyatt Inclusive Collection</lang>'),
+                (r'\bAlipay\b', r'<lang xml:lang="en-US">Alipay</lang>'),
+                (r'\bApple Pay\b', r'<lang xml:lang="en-US">Apple Pay</lang>'),
+                (r'\bWeChat Pay\b', r'<lang xml:lang="en-US">WeChat Pay</lang>'),
+                (r'\bCourtesy Card\b', r'<lang xml:lang="en-US">Courtesy Card</lang>'),
+                (r'\bclientes\b', r'<phoneme alphabet="ipa" ph="kli.jɑ̃t">clientes</phoneme>'),
+                (r'\bQu’allez-vous\b', r'<phoneme alphabet="ipa" ph="kalevu">Qu’allez-vous</phoneme>'),
+                (r'\bDevon\b', r'<phoneme alphabet="ipa" ph="devɔn">Devon</phoneme>'),
+                (r'\bplus\b', r'<phoneme alphabet="ipa" ph="ˈplys">plus</phoneme>'),
+        ]
+    },
+    "3": {
+        "name": "ja-JP",
+        "rules": [
+            (r'\bAvis\b', r'<phoneme alphabet="sapi" ph="エイビス">Avis</phoneme>'),
+        ]
+    },
+    "4": {
+        "name": "zh-CN",
+        "rules": [
+            (r'\bHyatt Vacation Club\b', r'<lang xml:lang="en-US">Hyatt Vacation Club</lang>'),
+            (r'\bMiraval\b', r'<lang xml:lang="en-US">Miraval</lang>'),
+            (r'\bApple Pay\b', r'<lang xml:lang="en-US">Apple Pay</lang>'),
+            (r'\bHyatt Studios\b', r'<lang xml:lang="en-US">Hyatt <phoneme alphabet="ipa" ph="ˈstjuːdioʊs">Studios</phoneme></lang>'),
+        ]
+    },
+}
